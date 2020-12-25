7278b60d1e5789f09164eb01ac5cd8cd2b0098cd
vforchi <vincenzo.forchi@gmail.com>
2020-12-25 20:59:11 +0100

    added day 7

---
+++ src/main/java/aoc/y2020/day07/Bag.java
@@ -0,0 +1,38 @@
+package aoc.y2020.day07;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class Bag {
+
+    private static Pattern pattern = Pattern.compile("([a-z ]+) contain ([0-9a-z, ]+)+\\.");
+
+    String color;
+    Map<String, Integer> containedBags;
+
+    public static Bag fromString(String string) {
+        var m = pattern.matcher(string.replaceAll(" bag[s]?", ""));
+        if (m.matches()) {
+            var color = m.group(1);
+            Map<String, Integer> containedBags = Collections.emptyMap();
+            if (!m.group(2).contains("no other")) {
+                containedBags = Arrays.stream(m.group(2).split(", "))
+                        .map(s -> s.split("(?<=[0-9]) "))
+                        .collect(Collectors.toMap(
+                                t -> t[1],
+                                t -> Integer.parseInt(t[0])
+                        ));
+            }
+            return new Bag(color, containedBags);
+        } else {
+            throw new RuntimeException("Can't parse bag " + string);
+        }
+    }
+
+}
---
+++ src/main/java/aoc/y2020/day07/Day07.java
@@ -0,0 +1,55 @@
+package aoc.y2020.day07;
+
+import aoc.Day;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day07 extends Day {
+
+    List<Bag> bags;
+
+    @Override
+    public Object partOne() {
+        return bags.stream()
+                .filter(this::containsShinyGold)
+                .count();
+    }
+
+    private boolean containsShinyGold(Bag bag) {
+        if (bag.containedBags.containsKey("shiny gold")) {
+            return true;
+        } else {
+            return bag.containedBags.keySet().stream()
+                    .map(this::findBagByColor)
+                    .anyMatch(this::containsShinyGold);
+        }
+    }
+
+    @Override
+    public Object partTwo() {
+        return countContainedBags(findBagByColor("shiny gold"));
+    }
+
+    private Long countContainedBags(Bag bag) {
+        return bag.containedBags.entrySet().stream()
+                .mapToLong(e -> e.getValue() * (1 + countContainedBags(findBagByColor(e.getKey()))))
+                .sum();
+    }
+
+    private Bag findBagByColor(String color) {
+        return bags.stream()
+                .filter(b -> b.color.equals(color))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    @Override
+    public void setup(String path) throws Exception {
+        super.setup(path);
+        this.bags = this.input.stream()
+                .map(Bag::fromString)
+                .collect(Collectors.toList());
+    }
+
+}
