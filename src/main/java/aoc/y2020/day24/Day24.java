37906dbfbfb8e0c4c9d863e74c99f0175cb4057e
vforchi <vincenzo.forchi@gmail.com>
2020-12-25 10:43:37 +0100

    added day 24

---
+++ src/main/java/aoc/y2020/day24/Day24.java
@@ -0,0 +1,93 @@
+package aoc.y2020.day24;
+
+import aoc.Day;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Day24 extends Day {
+
+    private Set<HexTile> blackTiles;
+
+    @Override
+    public Object partOne() {
+        return this.blackTiles.size();
+    }
+
+    @Override
+    public Object partTwo() {
+        IntStream.range(0, 100).forEach(i -> flipTiles(blackTiles));
+        return blackTiles.size();
+    }
+
+    private void flipTiles(Set<HexTile> blackTiles) {
+        var newBlackTiles = getWhiteNeighbours(blackTiles).stream()
+                .filter(t -> flipsBlack(t, blackTiles))
+                .collect(Collectors.toSet());
+
+        var newWhiteTiles = blackTiles.stream()
+                .filter(t -> flipsWhite(t, blackTiles))
+                .collect(Collectors.toSet());
+
+        blackTiles.removeAll(newWhiteTiles);
+        blackTiles.addAll(newBlackTiles);
+    }
+
+    private Set<HexTile> getWhiteNeighbours(Set<HexTile> blackTiles) {
+        return blackTiles.stream()
+                .flatMap(HexTile::getNeighbours)
+                .filter(t -> !blackTiles.contains(t))
+                .collect(Collectors.toSet());
+    }
+
+    private boolean flipsWhite(HexTile blackTile, Set<HexTile> blackTiles) {
+        var blackNeighbours = countBlackNeighbours(blackTile, blackTiles);
+        return blackNeighbours == 0 || blackNeighbours > 2;
+    }
+
+    private boolean flipsBlack(HexTile whiteTile, Set<HexTile> blackTiles) {
+        return countBlackNeighbours(whiteTile, blackTiles) == 2;
+    }
+
+    private long countBlackNeighbours(HexTile tile, Set<HexTile> blackTiles) {
+        return tile.getNeighbours()
+                .filter(blackTiles::contains)
+                .count();
+    }
+
+    @Override
+    public void setup(String path) throws Exception {
+        super.setup(path);
+
+        blackTiles = input.stream()
+                .map(this::toMovementsList)
+                .map(this::sumMovements)
+                .collect(
+                        Collectors.groupingBy(
+                                Function.identity(),
+                                Collectors.counting()
+                ))
+                .entrySet().stream()
+                .filter(e ->  e.getValue() % 2 == 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+    }
+
+    private List<HexTile> toMovementsList(String line) {
+        return Arrays.stream(line.split("(?<=(e|w))"))
+                .map(HexTile::fromString)
+                .collect(Collectors.toList());
+    }
+
+    private HexTile sumMovements(List<HexTile> movements) {
+        return movements.stream()
+                .reduce(HexTile::sum)
+                .orElseThrow();
+    }
+
+}
---
+++ src/main/java/aoc/y2020/day24/HexTile.java
@@ -0,0 +1,52 @@
+package aoc.y2020.day24;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+import java.util.stream.Stream;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+public class HexTile {
+
+    private final double east;
+    private final double north;
+
+    public Stream<HexTile> getNeighbours() {
+        return Stream.of(
+                this.add(new HexTile(1, 0)),
+                this.add(new HexTile(-1, 0)),
+                this.add(new HexTile(0.5, 0.5)),
+                this.add(new HexTile(0.5, -0.5)),
+                this.add(new HexTile(-0.5, 0.5)),
+                this.add(new HexTile(-0.5, -0.5))
+        );
+    }
+
+    public HexTile add(HexTile other) {
+        return new HexTile(this.east + other.east, this.north + other.north);
+    }
+
+    public static HexTile fromString(String movement) {
+        double east = 0.0, north = 0.0;
+
+        if (movement.contains("e")) {
+            east = 1.0 / movement.length();
+        } else if (movement.contains("w")) {
+            east = -1.0 / movement.length();
+        }
+
+        if (movement.contains("n")) {
+            north = 0.5;
+        } else if (movement.contains("s")) {
+            north = -0.5;
+        }
+
+        return new HexTile(east, north);
+    }
+
+    public static HexTile sum(HexTile tile1, HexTile tile2) {
+        return tile1.add(tile2);
+    }
+
+}
