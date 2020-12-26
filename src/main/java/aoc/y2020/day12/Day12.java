6dbd0c61a63233f71a2a07a34617b1f679a861ea
vforchi <vincenzo.forchi@gmail.com>
2020-12-26 17:42:57 +0100

    added day 12

---
+++ src/main/java/aoc/y2020/day12/Day12.java
@@ -0,0 +1,36 @@
+package aoc.y2020.day12;
+
+import aoc.Day;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day12 extends Day {
+
+	List<Command> commands;
+
+	@Override
+	public Object partOne() {
+		var ship = new Ship(0, 0);
+		commands.forEach(c -> c.apply(ship));
+		return ship.toString();
+	}
+
+	@Override
+	public Object partTwo() {
+		var ship = new Ship(0, 0);
+		var waypoint = new Waypoint(10, 1);
+		commands.forEach(c -> c.apply(ship, waypoint));
+		return ship.toString();
+	}
+
+	@Override
+	public void setup(String path) throws Exception {
+		super.setup(path);
+
+		commands = input.stream()
+				.map(Command::fromString)
+				.collect(Collectors.toList());
+	}
+
+}
