75a6857d56b8f22949da9962a3642016bb729938
vforchi <vincenzo.forchi@gmail.com>
2020-12-28 22:39:04 +0100

    improved performance of day 15

--- src/main/java/me/vforchi/aoc/y2020/day15/Day15.java
+++ src/main/java/me/vforchi/aoc/y2020/day15/Day15.java
@@ -2,9 +2,6 @@ package me.vforchi.aoc.y2020.day15;
 
 import me.vforchi.aoc.Day;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Day15 extends Day {
 
 	@Override
@@ -18,7 +15,13 @@ public class Day15 extends Day {
 	}
 
 	private static Integer getNumber(int size) {
-		Map<Integer, Integer> numbers = new HashMap<>(Map.of(16, 1, 11, 2, 15, 3, 0, 4, 1, 5));
+//		Map<Integer, Integer> numbers = new HashMap<>(Map.of(16, 1, 11, 2, 15, 3, 0, 4, 1, 5));
+		Integer[] numbers = new Integer[size];
+		numbers[16] = 1;
+		numbers[11] = 2;
+		numbers[15] = 3;
+		numbers[0] = 4;
+		numbers[1] = 5;
 
 		var nextNumber = 7;
 		var round = 5;
@@ -29,12 +32,10 @@ public class Day15 extends Day {
 		return nextNumber;
 	}
 
-	private static int next(Map<Integer, Integer> numbers, int lastNumber, int round) {
-		var nextNumber = 0;
-		if (numbers.containsKey(lastNumber)) {
-			nextNumber = round - numbers.get(lastNumber);
-		}
-		numbers.put(lastNumber, round);
+	private static int next(Integer[] numbers, int lastNumber, int round) {
+		var lastNumberRound = numbers[lastNumber];
+		var nextNumber = (lastNumberRound == null) ? 0 : (round - lastNumberRound);
+		numbers[lastNumber] = round;
 		return nextNumber;
 	}
 
