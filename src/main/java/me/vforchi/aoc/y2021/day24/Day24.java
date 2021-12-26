8b05e7d4bea222cd3bcd3f2b88b592084401aca5
vforchi <vincenzo.forchi@gmail.com>
2021-12-26 11:52:23 +0100

    added day 24

---
+++ src/main/java/me/vforchi/aoc/y2021/day24/Day24.java
@@ -0,0 +1,17 @@
+package me.vforchi.aoc.y2021.day24;
+
+import me.vforchi.aoc.Day;
+
+public class Day24 extends Day {
+
+    @Override
+    public Object partOne() {
+        return "Solved by hand";
+    }
+
+    @Override
+    public Object partTwo() {
+        return "Solved by hand";
+    }
+
+}
